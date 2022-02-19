718d19ed9ebf20e4776840c6a2522e281abe1e26
Lucas Dias <lucas_dias@redes.ufsm.br>
2022-02-19 16:15:51 -0300

    Add documentation of some interface.
    Signed-off-by: Lucas Dias <lucas_dias@redes.ufsm.br>

--- include/Interface.hpp
+++ include/Interface.hpp
@@ -2,7 +2,7 @@
  * Interface.hpp
  *
  *  Created on: 14 de ago de 2020
- *      Author: root
+ *      Author: Lucas Dias
  */
 
 #ifndef INTERFACE_HPP_
@@ -11,31 +11,104 @@
 
 namespace InterfacePKI
 {
+
+//<! Interface to encrypt/decrypt method
 class Confidentiality
 {
 public :
+
+	/*
+	 * \brief Constructor
+	 */
 	Confidentiality()=default;
-	std::string virtual EncryptMessage(const std::string& ) =0;
-	std::string virtual DecryptMessage(const std::string& ) =0;
+
+	/*
+	 *\brief signature of encrypt method.
+	 *\param plain String to be encrypted.
+	 *\return Message encrypted
+	 */
+	std::string virtual EncryptMessage(const std::string& plain) =0;
+
+	/*
+	 *\brief signature of decrypt method.
+	 *\param encrypted String to be decrypted.
+	 *\return Text plain
+	 */
+	std::string virtual DecryptMessage(const std::string& encrypted) =0;
+
+	/*
+	 * \brief Destructor
+	 */
 	virtual ~Confidentiality()=default;
 };
+
+//<! Interface to the symmetric key cryptography
 class SymmetricKey
 {
 public:
-	void virtual ConfigureKey(std::string) =0;
-	void virtual ConfigureIV(std::string) =0;
 
+	/*
+	 * \brief Constructor
+	 */
+	SymmetricKey()=default;
+
+	/*
+	 *\brief Recovery the symmetric key to a std::string container.
+	 *\return Symmetric key string.
+	 */
+	std::string virtual RecoveryKey() =0;
+
+	/*
+	 *\brief Recovery the initialization vector (IV) to a std::string container.
+	 *\return Initialization Vector, or an empty string.
+	 */
+	std::string virtual RecoveryIV() =0;
+
+	/*
+	 *\brief Configure the symmetric key to be used.
+	 *\param new_key The new key to be used.
+	 */
+	void virtual ConfigureKey(std::string new_key) =0;
+
+	/*
+	 *\brief Configure the initialization vector to be used.
+	 *\param new_key The initialization vector to be used.
+	 */
+	void virtual ConfigureIV(std::string new_iv) =0;
+
+	/*
+	 * \brief Destructor
+	 */
 	virtual ~SymmetricKey()=default;
 };
 
+//<! Interface to the authentication mode.
 class AuthenticationMode
 {
 public:
+
+	/*
+	 *\brief Verify the MAC from text.
+	 *\param Text string to be MAC verified.
+	 *\param MAC message authentication code.
+	 *\return True, if MAC is correct, false otherwise.
+	 */
 	bool virtual VerifyMAC(std::string text, std::string mac)=0;
+
+	/*
+	 *\brief Compute the MAC from a text.
+	 *\param text Input of the MAC generation.
+	 *\return The message authentication code.
+	 */
 	std::string virtual  GenerateMAC(std::string text) =0;
+
+	/*
+	 *\breif Destructor
+	 */
 	virtual ~AuthenticationMode()=default;
 };
 
+//<! Interface to the symmetric key cryptography
 class ConfidentialityAuth
 {
 public :
@@ -44,6 +117,7 @@ public :
 	virtual ~ConfidentialityAuth() =default;
 };
 
+//<! Interface to the simmetric key cryptography
 class CipherAuthenticationMode
 {
 public:
@@ -51,6 +125,8 @@ public:
 	virtual bool DecryptAuth(std::string ciphertext, std::string&plaintext) =0;
 	virtual ~CipherAuthenticationMode()=default;
 };
+
+//<! Interface to the signature digital operations
 class SignatureDigital
 {
 public :
@@ -60,6 +136,7 @@ public :
 	virtual ~SignatureDigital()=default;
 };
 
+//<! Interface to StreamStore. Save public/private keys to files or export them to std::string.
 class StreamStore
 {
 public:
@@ -71,6 +148,6 @@ public:
 	virtual ~StreamStore()=default;
 };
 
-}
+} //namespace InterfacePKI
 
 #endif /* INTERFACE_HPP_ */
